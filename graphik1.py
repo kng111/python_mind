8bf4d495c20bc93dc53439b70843524c12b1ebf9
Vladislav <yak.vlad@bk.ru>
2023-07-11 21:09:08 +0300

    /

--- graphik1.py
+++ graphik1.py
@@ -1,92 +1,79 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Загрузка исторических данных
+historical_data = pd.read_csv('historical_data.csv')
+
+# Загрузка предсказанных данных
+predicted_data = pd.read_csv('predicted_data.csv')
+
+# Извлечение дат и курсов
+timestamps = historical_data['Date'].tolist()
+prices = historical_data['USD_RUB'].tolist()
+predicted_prices = predicted_data['USD_RUB'].tolist()
+
+# Функция для отображения графика
+def plot_currency(timestamps, prices, currency_name, predicted_prices=None):
+    plt.figure(figsize=(10, 6))
+    plt.plot(timestamps, prices, marker='o', markersize=6, linewidth=2, color='white', label='Реальная цена')
+    if predicted_prices:
+        plt.plot(timestamps[-len(predicted_prices):], predicted_prices, linestyle='--', linewidth=2, color='red', label='Предсказанная цена')
+    plt.xlabel('Дата', fontsize=10)
+    plt.ylabel('USD_RUB', fontsize=12)
+    plt.title(f'Курс {currency_name}', fontsize=14)
+    plt.xticks(rotation=45, ha='right')
+    plt.gca().set_facecolor('black')
+    plt.grid(color='white', linestyle='--', linewidth=0.5)
+    plt.legend(fontsize=12)
+    plt.grid(False)
+    plt.tight_layout()
+    plt.show()
+
+# Отображение графика
+plot_currency(timestamps, prices, 'доллара', predicted_prices)
+
+
 # import numpy as np
+# import pandas as pd
 # import matplotlib.pyplot as plt
 
-# # Исторические данные курсов доллара и евро
-# timestamps = list(range(1, 31))  # Пример временных меток
-# dollars = [73.25, 73.45, 73.55, 73.65, 73.75, 73.85, 73.95, 73.85, 73.95, 73.85, 73.75, 73.65, 73.75, 73.85, 73.95,
-#            73.85, 73.95, 73.85, 73.75, 73.65, 73.55, 73.45, 73.55, 73.65, 73.75, 73.85, 73.95, 73.85, 73.95, 73.85]
+# # Загрузка исторических данных
+# historical_data = pd.read_csv('historical_data.csv')
 
-# euros = [87.12, 87.28, 87.35, 87.42, 87.58, 87.75, 87.82, 87.75, 87.82, 87.75, 87.58, 87.42, 87.58, 87.75, 87.82,
-#          87.75, 87.82, 87.75, 87.58, 87.42, 87.35, 87.28, 87.35, 87.42, 87.58, 87.75, 87.82, 87.75, 87.82, 87.75]
+# # Загрузка предсказанных данных
+# predicted_data = pd.read_csv('predicted_data.csv')
 
-# # Функция для отображения графика
-# def plot_currency(timestamps, prices, currency_name, predicted_prices=None, start_day=None, end_day=None):
-#     plt.figure(figsize=(10, 6))  # Размер графика
-#     plt.plot(timestamps, prices, marker='o', markersize=6, linewidth=2, color='white', label='Реальная цена')  # Стиль линии и маркеры
+# # Извлечение дат и курсов
+# timestamps = pd.to_datetime(historical_data['Date']).dt.date.tolist()
+# prices = historical_data['USD_RUB'].astype(float).tolist()
+# predicted_prices = predicted_data['USD_RUB'].astype(float).tolist()
+
+# # Функция для отображения графика с временными отрезками покупки и продажи
+# def plot_currency_with_trading(timestamps, prices, currency_name, predicted_prices=None):
+#     plt.figure(figsize=(10, 6))
+#     plt.plot(timestamps, prices, marker='o', markersize=6, linewidth=2, color='blue', label='Реальная цена')
 #     if predicted_prices:
-#         plt.plot(timestamps[:end_day], predicted_prices[start_day-1:end_day], linestyle='--', linewidth=2, color='orange', label='Предсказанная цена')
-#         for i in range(start_day-1, end_day):
-#             if np.isnan(predicted_prices[i]):
-#                 continue
-#             if predicted_prices[i] >= prices[i]:
-#                 plt.fill_between([timestamps[i], timestamps[i+1]], [prices[i], prices[i+1]], [predicted_prices[i], predicted_prices[i+1]], facecolor='green', alpha=0.3)
-#             else:
-#                 plt.fill_between([timestamps[i], timestamps[i+1]], [prices[i], prices[i+1]], [predicted_prices[i], predicted_prices[i+1]], facecolor='red', alpha=0.3)
-#     plt.xlabel('Временная метка', fontsize=12)  # Размер шрифта осей
-#     plt.ylabel('Цена', fontsize=12)
-#     plt.title(f'Курс {currency_name}', fontsize=14)  # Размер шрифта заголовка
-#     plt.xticks(fontsize=10)  # Размер шрифта меток на осях
-#     plt.yticks(fontsize=10)
-#     plt.grid(True, linestyle='--', alpha=0.5)  # Сетка со стилем пунктирной линии
-#     plt.gca().set_facecolor('black')  # Цвет фона графика
-#     plt.tight_layout()  # Автоматическое выравнивание
+#         plt.plot(timestamps[-len(predicted_prices):], predicted_prices, linestyle='--', linewidth=2, color='orange', label='Предсказанная цена')
+#         buy_indices = np.where(np.array(predicted_prices[:-1]) < np.array(predicted_prices[1:]) - 0.5)[0] + len(prices) - 1
+#         sell_indices = np.where(np.array(predicted_prices[:-1]) > np.array(predicted_prices[1:]) + 0.5)[0] + len(prices) - 1
+#         if len(buy_indices) > 0 and len(sell_indices) > 0:
+#             plt.axvspan(timestamps[buy_indices[0]], timestamps[sell_indices[0]], ymin=0, ymax=max(prices), facecolor='green', alpha=0.3)
+#             for i in range(1, min(len(buy_indices), len(sell_indices))):
+#                 plt.axvspan(timestamps[sell_indices[i-1]], timestamps[buy_indices[i]], facecolor='red', alpha=0.3)
+#             if len(buy_indices) > len(sell_indices):
+#                 plt.axvspan(timestamps[buy_indices[-1]], timestamps[-1], facecolor='red', alpha=0.3)
+#             elif len(buy_indices) < len(sell_indices):
+#                 plt.axvspan(timestamps[sell_indices[-1]], timestamps[-1], facecolor='green', alpha=0.3)
+#     plt.xlabel('Дата', fontsize=12)
+#     plt.ylabel('USD_RUB', fontsize=12)
+#     plt.title(f'Курс {currency_name} с временными отрезками покупки и продажи', fontsize=14)
+#     plt.xticks(rotation=45, ha='right')
+#     plt.gca().set_facecolor('black')
+#     plt.grid(color='white', linestyle='--', linewidth=0.5)
+#     plt.legend(fontsize=12)
+#     plt.tight_layout()
 #     plt.show()
 
-# # Отображение графика курса доллара
-# plot_currency(timestamps, dollars, 'доллара')
-
-# # Отображение графика курса евро
-# # plot_currency(timestamps, euros, 'евро')
-
-# # Изменение временной метки и обновление графиков
-# new_timestamps = list(range(1, 31 + 7))  # Новая временная метка на 7 дней вперед
-
-# predictions_dollars = [73.65, 73.75, 73.85, 73.95, 73.85, 73.95, 73.85, 73.75, 73.65, 73.55, 73.45, 73.55, 73.65, 73.75,
-#                        73.85, 73.75, 73.65, 73.75, 73.85, 73.95, 73.85, 73.75, 73.65, 73.75, 73.85, 73.95, 73.85, 73.95,
-#                        73.85, 73.75, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]  # Пример предсказанных цен доллара
-
-# # Обновление графика курса доллара с новыми данными и ограничением промежутка от 1 до 30 дня
-# plot_currency(new_timestamps, dollars + [np.nan] * 7, 'доллара', predicted_prices=predictions_dollars, start_day=1, end_day=30)
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
-
-# Исторические данные курса доллара (приближенные к реальным)
-timestamps = list(range(1, 31))
-dollars = [73.25, 73.45, 73.55, 73.65, 73.75, 73.85, 73.95, 73.85, 73.95, 73.85, 73.75, 73.65, 73.75, 73.85, 73.95,
-           73.85, 73.95, 73.85, 73.75, 73.65, 73.55, 73.45, 73.55, 73.65, 73.75, 73.85, 73.95, 73.85, 73.95, 73.85]
-
-# Функция для предсказания курса доллара на заданный период
-def predict_dollar(timestamps, dollars, forecast_steps):
-    model = ARIMA(dollars, order=(1, 1, 1))
-    model_fit = model.fit()
-
-    forecast = model_fit.forecast(steps=forecast_steps)[0]
-
-    return forecast
-
-# Прогноз на 4 недели (28 дней)
-forecast_steps = 28
-forecast = predict_dollar(timestamps, dollars, forecast_steps)
-
-# Отображение графика прогноза
-plt.figure(figsize=(10, 6))
-plt.plot(timestamps, dollars, marker='o', markersize=6, linewidth=2, color='white', label='Реальная цена')
-plt.plot(np.arange(len(timestamps), len(timestamps) + len(forecast)), forecast, linestyle='--', linewidth=2, color='orange', label='Прогноз')
-plt.xlabel('Временная метка', fontsize=12)
-plt.ylabel('Цена', fontsize=12)
-plt.title('Прогноз курса доллара', fontsize=14)
-plt.xticks(fontsize=10)
-plt.yticks(fontsize=10)
-plt.grid(True, linestyle='--', alpha=0.5)
-plt.gca().set_facecolor('black')
-plt.tight_layout()
-plt.legend(fontsize=12)
-plt.show()
-
-# Вывод прогнозных значений
-print("Прогноз курса доллара:")
-for i, value in enumerate(forecast):
-    print(f"День {i+1}: {value:.2f}")
+# # Отображение графика с временными отрезками покупки и продажи
+# plot_currency_with_trading(timestamps, prices, 'доллара', predicted_prices)
--- zapolnutCSV.py
+++ zapolnutCSV.py
@@ -28,16 +28,16 @@ import random
 import csv
 
 # Открываем файл historical_data.csv для записи
-with open('historical_data.csv', 'w', newline='') as file:
+with open('historical_data copy.csv', 'w', newline='') as file:
     writer = csv.writer(file)
 
     # Записываем заголовок
-    writer.writerow(['Date', 'USD_RUB'])
+    writer.writerow(['USD_RUB'])
 
     # Генерируем случайные значения для USD_RUB и записываем их в файл
     for month in range(7, 13):  # Генерация значений для 3 месяцев (1, 2, 3)
         for day in range(1, 31):  # Генерация значений для 30 дней в каждом месяце
-            date = f"2023-{month:02d}-{day:02d}"  # Генерация даты в формате "2023-01-01", "2023-01-02" и т.д.
+            # date = f"2023-{month:02d}-{day:02d}"  # Генерация даты в формате "2023-01-01", "2023-01-02" и т.д.
             usd_rub = random.uniform(70.0, 85.0)  # Генерация случайного значения в диапазоне от 70.0 до 77.0
-            writer.writerow([date, str(usd_rub)])  # Преобразуем usd_rub в строку
+            writer.writerow([ str(usd_rub)])  # Преобразуем usd_rub в строку
 
